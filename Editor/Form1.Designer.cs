310f3bddc688f7c0ea2081c4c26e9cdd017839f5
sergiorps <sergiorps@ig.com.br>
2017-09-15 10:35:20 -0300

    Update

---
+++ Editor/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textoConteudo = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.botaoGrava = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.botaoBusca = new System.Windows.Forms.Button();
+            this.textoBusca = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textReplace = new System.Windows.Forms.TextBox();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textoConteudo
+            // 
+            this.textoConteudo.Location = new System.Drawing.Point(10, 11);
+            this.textoConteudo.Multiline = true;
+            this.textoConteudo.Name = "textoConteudo";
+            this.textoConteudo.Size = new System.Drawing.Size(569, 288);
+            this.textoConteudo.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // botaoGrava
+            // 
+            this.botaoGrava.Location = new System.Drawing.Point(12, 382);
+            this.botaoGrava.Name = "botaoGrava";
+            this.botaoGrava.Size = new System.Drawing.Size(75, 23);
+            this.botaoGrava.TabIndex = 2;
+            this.botaoGrava.Text = "Gravar";
+            this.botaoGrava.UseVisualStyleBackColor = true;
+            this.botaoGrava.Click += new System.EventHandler(this.botaoGrava_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnReplace);
+            this.groupBox1.Controls.Add(this.textReplace);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textoBusca);
+            this.groupBox1.Controls.Add(this.botaoBusca);
+            this.groupBox1.Location = new System.Drawing.Point(303, 305);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Find and Replace";
+            // 
+            // botaoBusca
+            // 
+            this.botaoBusca.Location = new System.Drawing.Point(80, 71);
+            this.botaoBusca.Name = "botaoBusca";
+            this.botaoBusca.Size = new System.Drawing.Size(75, 23);
+            this.botaoBusca.TabIndex = 0;
+            this.botaoBusca.Text = "Find";
+            this.botaoBusca.UseVisualStyleBackColor = true;
+            this.botaoBusca.Click += new System.EventHandler(this.botaoBusca_Click);
+            // 
+            // textoBusca
+            // 
+            this.textoBusca.Location = new System.Drawing.Point(80, 19);
+            this.textoBusca.Name = "textoBusca";
+            this.textoBusca.Size = new System.Drawing.Size(179, 20);
+            this.textoBusca.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Find:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Replace:";
+            // 
+            // textReplace
+            // 
+            this.textReplace.Location = new System.Drawing.Point(80, 49);
+            this.textReplace.Name = "textReplace";
+            this.textReplace.Size = new System.Drawing.Size(179, 20);
+            this.textReplace.TabIndex = 4;
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(184, 71);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(75, 23);
+            this.btnReplace.TabIndex = 5;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 417);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.botaoGrava);
+            this.Controls.Add(this.textoConteudo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textoConteudo;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button botaoGrava;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textoBusca;
+        private System.Windows.Forms.Button botaoBusca;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.TextBox textReplace;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Editor/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("texto.txt"))
+            {
+                Stream entrada = File.Open("texto.txt", FileMode.Open);
+                StreamReader leitor = new StreamReader(entrada);
+                string linha = leitor.ReadLine();
+                while (linha != null)
+                {
+                    textoConteudo.Text += linha;
+                    linha = leitor.ReadLine();
+                }
+                leitor.Close();
+                entrada.Close();
+            }
+            else
+            {
+                MessageBox.Show("Não rolou");
+            }
+        }
+
+        private void botaoGrava_Click(object sender, EventArgs e)
+        {
+            Stream saida = File.Open("texto.txt", FileMode.Create);
+            StreamWriter escritor = new StreamWriter(saida);
+            escritor.Write(textoConteudo.Text);
+            escritor.Close();
+            saida.Close();
+        }
+
+        private void botaoBusca_Click(object sender, EventArgs e)
+        {
+            string busca = textoBusca.Text;
+            string textoDoEditor = textoConteudo.Text;
+            int resultado = textoDoEditor.IndexOf(busca);
+            if (resultado >= 0)
+            {
+                MessageBox.Show("achei o texto " + busca);
+            }
+            else
+            {
+                MessageBox.Show("não achei");
+            }
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            string busca = textoBusca.Text;
+            string textoDoEditor = textoConteudo.Text;
+            string replace = textReplace.Text;
+              
+            int resultado = textoDoEditor.IndexOf(busca);
+
+            if (resultado >= 0)
+            {
+                textoConteudo.Text = textoDoEditor.Replace(busca, replace);
+                MessageBox.Show("Texto: " + busca + " trocado por: " + replace);
+            }
+            else
+            {
+                MessageBox.Show("não achei");
+            }
+            
+        }
+    }
+}
